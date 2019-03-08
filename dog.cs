103e604edd67492ee98cc8f5bf2e789180ca19ed
韩镇先 <hanzhenxian@hanzhenxiandeMacBook-Pro.local>
2019-03-08 17:34:07 -0600

    complete

---
+++ dog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Pet
+{
+    public class dog
+    {
+
+
+        public string name;
+        public string owner;
+        public string weight;
+
+
+
+        public dog( string name, string owner,string weight )
+        {
+            this.name = name;
+            this.owner = owner;
+            this.weight = weight;
+         
+        }
+
+        public void Bark(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Console.Write("bark!");
+            }
+            Console.WriteLine("");
+        }
+
+        public string GetTag()
+        {
+
+            return String.Format("If lost, call {0}." ,
+                                 owner);
+        }
+    }
+}
