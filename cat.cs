0aeca9903f89598b72183abf1211e71119f261a6
韩镇先 <hanzhenxian@hanzhenxiandeMacBook-Pro.local>
2019-03-08 17:33:26 -0600

    complete

---
+++ cat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Pet
+{
+    public class cat
+    {
+
+        public string owner;
+        public string name;
+        public string weight;
+       
+
+
+        public cat( string name, string owner, string weight )
+        {
+            this.name = name;
+            this.owner = owner;
+            this.weight = weight;
+        }
+
+        public void Meow(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Console.Write("Meow.");
+            }
+            Console.WriteLine("");
+        }
+
+        public string GetTag()
+        {
+
+            return String.Format("If lost, call {0}." ,
+                                 owner);
+        }
+    }
+}
