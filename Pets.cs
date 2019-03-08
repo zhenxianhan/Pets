fc184f037f42987895d099436b053e5c5a2dc077
韩镇先 <hanzhenxian@hanzhenxiandeMacBook-Pro.local>
2019-03-08 17:34:34 -0600

    complete

---
+++ Pets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Pet
+{
+    public class Pets
+    {
+        public string type;
+        public string name;
+        public string owner;
+        public string weight;
+
+
+
+        public Pets( string type, string name, string owner, string weight )
+        {
+            this.type = type;
+            this.name = name;
+            this.owner = owner;
+            this.weight = weight;
+
+        }
+
+       
+        public string GetTag()
+        {
+
+            return String.Format("If lost, call {0}." ,
+                                 owner);
+        }
+    }
+}
